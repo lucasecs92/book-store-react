cf6c954a7cdfd55b5009e679191c793a8d8aec3f
Lucas Eduardo <102336205+lucasecs92@users.noreply.github.com>
2023-12-06 15:01:26 -0300

    formulário de login
    finalizado

--- book-store/src/assets/components/FormLogin.jsx
+++ book-store/src/assets/components/FormLogin.jsx
@@ -1,31 +1,46 @@
-import { useEffect, useRef} from 'react';
+import { useEffect, useRef, useState} from 'react';
 import styles from '../css/FormLogin.module.css';
+
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 const FormLogin = () => {
 
     const formSigninRef = useRef(null);
     const formSignupRef = useRef(null);
     const btnColorRef = useRef(null);
+
+    const [showPassword, setShowPassword] = useState(false);
     
-    // const [textColorSignin, setTextColorSignin] = useState('white');
-    // const [textColorSignup, setTextColorSignup] = useState('black');
+    const [selectedButton, setSelectedButton] = useState(null);
+    const [signinColor, setSigninColor] = useState('white');
+    const [signupColor, setSignupColor] = useState('black');
 
     useEffect(() => {
         const btnSignin = document.querySelector('#btnSignin');
         const btnSignup = document.querySelector('#btnSignup');
  
         btnSignin.addEventListener('click', () => {
+            setSelectedButton('signin');
             formSigninRef.current.style.left = "25px";
             formSignupRef.current.style.left = "450px";
             btnColorRef.current.style.left = "0px";
+            setSigninColor('white'); // Altere para a cor que você deseja
+            setSignupColor('black'); // Altere para a cor que você deseja
         });
  
         btnSignup.addEventListener('click', () => {
+            setSelectedButton('signup');
             formSigninRef.current.style.left = "-450px";
             formSignupRef.current.style.left = "25px";
             btnColorRef.current.style.left = "110px";
+            setSigninColor('black'); // Altere para a cor que você deseja
+            setSignupColor('white'); // Altere para a cor que você deseja
         });
     }, []);
+
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
  
     return (
         <>
@@ -33,15 +48,18 @@ const FormLogin = () => {
                 
                 <div className={styles.buttonsForm}>
                     <div className={styles.btnColor} ref={btnColorRef}></div>
-                    <button id="btnSignin" type="button">Sign In</button>
-                    <button id="btnSignup" type="button">Sign Up</button>
+                    <button id="btnSignin" type="button" className={selectedButton === 'signin' ? 'selected' : ''} style={{color: signinColor}}>Sign In</button>
+                    <button id="btnSignup" type="button" className={selectedButton === 'signup' ? 'selected' : ''} style={{color: signupColor}}>Sign Up</button>
                 </div>
 
                 <form className={styles.signIn} ref={formSigninRef}>
 
                     <input className={styles.inputText} id='input-text' type="text" placeholder="Email" required />
 
-                    <input className={styles.inputPassword} type="password" placeholder="Password" required />
+                    <div className={styles.divInputPsw}>
+                        <input className={styles.inputPassword} type={showPassword ? "text" : "password"} placeholder="Password" required />
+                        {showPassword ? <FaRegEyeSlash onClick={handleShowPassword}/> : <FaRegEye onClick={handleShowPassword}/>}    
+                    </div>
                     
                     <div className={styles.divCheck}>
                         <input type="checkbox" id='remember-me'/>
@@ -55,11 +73,14 @@ const FormLogin = () => {
 
                 <form className={styles.signUp} ref={formSignupRef}>
 
+                    <input className={styles.inputText} type="text" placeholder='Nome completo' required />
+
                     <input className={styles.inputText} type="text" placeholder="Email" required />
 
-                    <input className={styles.inputPassword} type="password" placeholder="Create Password" required />
-
-                    <input className={styles.inputPassword} type="password" placeholder="Confirm Password" required />
+                    <div className={styles.divInputPsw}>
+                        <input className={styles.inputPassword} type={showPassword ? "text" : "password"} placeholder="Create Password" required />
+                        {showPassword ? <FaRegEyeSlash onClick={handleShowPassword}/> : <FaRegEye onClick={handleShowPassword}/>}
+                    </div>
 
                     <div className={styles.divCheck}>
                         <input type="checkbox" required id='i-agree'/>
