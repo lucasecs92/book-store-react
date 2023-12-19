7e18a42629150db0162a6fd982da5d1395781f9e
Lucas Eduardo <102336205+lucasecs92@users.noreply.github.com>
2023-12-19 01:09:52 -0300

    up

--- book-store/src/assets/components/DownloadApp.jsx
+++ book-store/src/assets/components/DownloadApp.jsx
@@ -1,13 +1,13 @@
 import styles from '../css/DownloadApp.module.css';
 
 import telefone from '../images/mobile-bookstore.png';
-import googlePlayImg from '../images/google-play.jpg';
-import appStoreImg from '../images/app-store.jpg';
+import googlePlayImg from '../images/google-play.png';
+import appStoreImg from '../images/app-store.png';
 
 const DownloadApp = () => {
 
     return (
-        <>
+        <>   
             <section className={styles.downloadApp} id='downloadApp'>
                 <section className={styles.downloadAppWrap}>
 
@@ -19,8 +19,8 @@ const DownloadApp = () => {
                         <h2>Download Our App Now!</h2>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae dolore explicabo, architecto eveniet hic ipsum, ex provident omnis iste exercitationem repudiandae. At consequuntur reiciendis illo exercitationem cupiditate culpa et blanditiis.</p>
                         <div className={styles.apps}>
-                            <img src={googlePlayImg} alt="Google Play" />
-                            <img src={appStoreImg} alt="App Store" />
+                            <img src={googlePlayImg} alt="Google Play" className={styles.googlePlay}/>
+                            <img src={appStoreImg} alt="App Store" className={styles.appStore}/>
                         </div>
                     </div>
 
